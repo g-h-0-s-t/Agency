386dea2de5d0758a571792b6f9b9961172ea84e0
sol <discounttechwarehouse@gmail.com>
2020-06-13 15:04:54 +0000

    deploy build to gcloud app

---
+++ build/precache-manifest.b2d1a001801ed3c02615268c42a98697.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579d5eb5c5415e402a37c9549823f2ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27a5729dd73e227a04bd",
+    "url": "/static/css/main.2466229f.chunk.css"
+  },
+  {
+    "revision": "09e98bae4ad85c9634d7",
+    "url": "/static/js/2.cbc1bba3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cbc1bba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a5729dd73e227a04bd",
+    "url": "/static/js/main.817bc330.chunk.js"
+  },
+  {
+    "revision": "b2abcd83fca9b8182255",
+    "url": "/static/js/runtime-main.ca44497b.js"
+  },
+  {
+    "revision": "e32faa01b2529a27a34c26c78e43e07d",
+    "url": "/static/media/demo-1.e32faa01.png"
+  },
+  {
+    "revision": "eb5c9f1499f3cbc59582fea35d529b40",
+    "url": "/static/media/hero-1.eb5c9f14.svg"
+  }
+]);
